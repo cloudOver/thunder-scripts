e2d260851f7d6261010b784b09168315099dd6cd
Maciej Nabozny <maciej.nabozny@cloudover.io>
2016-07-13 21:35:55 +0200

    update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.command.install import install as _install
 setup(
   name = 'thunderscript',
   packages = find_packages(exclude=['config', 'config.*']),
-  version = '16.07.09',
+  version = '16.07.10',
   description = 'Thunder script parser',
   author = 'Marta Nabozny',
   author_email = 'maciej.nabozny@cloudover.io',
